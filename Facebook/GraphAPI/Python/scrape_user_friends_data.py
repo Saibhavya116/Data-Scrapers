ec52cd82f41ad0baf07a4b3379da714ed987d067
Saibhavya116 <bhavyareddy.pappireddy@gmail.com>
2018-03-11 20:03:02 -0400

    Initial scraping query

---
+++ Facebook/GraphAPI/Python/scrape_user_friends_data.py
@@ -0,0 +1,7 @@
+#!/usr/local/bin/python3.6
+
+import facebook
+
+graph = facebook.GraphAPI(access_token='EAACEdEose0cBACok2rDjPPZCm7sG3qJvCcdJkxYKu7ZBkEnKOGBsF63RF7nTUWZBQZA5x0d1N5nxDCFbzDdpxI40hzT7ddlv2CHIIYVv1HypSEBCxTZBhNqZBjeRFWd77Rsf8VbSqnox6AJKuTmPjvFT3jZCqV3JrSSzzLPY3KdYRNlbv8zoM8PJSbcmXRCWDsZD',version='2.10')
+events=graph.request('me?fields=id,name,friends.limit(100){name,likes.limit(150){place_topics{name},category,fan_count,global_brand_page_name}}') 
+
