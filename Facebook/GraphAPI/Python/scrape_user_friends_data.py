2af202082560beb2f0982a57d6b012550c8b05cb
Saibhavya116 <bhavyareddy.pappireddy@gmail.com>
2018-03-11 20:05:37 -0400

    Update scrape_user_friends_data.py

--- Facebook/GraphAPI/Python/scrape_user_friends_data.py
+++ Facebook/GraphAPI/Python/scrape_user_friends_data.py
@@ -2,6 +2,6 @@
 
 import facebook
 
-graph = facebook.GraphAPI(access_token='EAACEdEose0cBACok2rDjPPZCm7sG3qJvCcdJkxYKu7ZBkEnKOGBsF63RF7nTUWZBQZA5x0d1N5nxDCFbzDdpxI40hzT7ddlv2CHIIYVv1HypSEBCxTZBhNqZBjeRFWd77Rsf8VbSqnox6AJKuTmPjvFT3jZCqV3JrSSzzLPY3KdYRNlbv8zoM8PJSbcmXRCWDsZD',version='2.10')
+graph = facebook.GraphAPI(access_token='EAACEdEose0cBACok2rDjPPZCm7sG3qJvCcdJkxYKu7ZBkEnKOGBsF63RF7nTUWZBQA5x0d1N5nxDCFbzDdpxI40hzT7ddlv2CHIIYVv1HypSEBCxTZBhNqZBjeRFWd77Rsf8VbSqnox6AJKuTmPjvFT3jZCqV3JrSSzzLPY3KdYRNlbv8zoM8PJSbcmXRCWDsZD',version='2.10')
 events=graph.request('me?fields=id,name,friends.limit(100){name,likes.limit(150){place_topics{name},category,fan_count,global_brand_page_name}}') 
 
